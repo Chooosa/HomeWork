8272e4af2d4c8cf3cde7e806bcd7f676eeeb173f
Admin <kotlerroman@mail.ru>
2020-05-31 23:28:32 +0300

    Bug fix HW20

--- Homework20/validate.js
+++ Homework20/validate.js
@@ -1,7 +1,6 @@
 function validate(data) {
     const { login, password, confirmPassword,
         license, firstName, gender } = data;
-    let resp = 'ый';
 
     if (!login || !password) {
         alert('Укажите логин/пароль');
@@ -18,43 +17,7 @@ function validate(data) {
     } else if (!license) {
         alert('Необходимо согласие');
     } else {
-        if (gender == 'female') {
-            resp = 'ая';
-        }
-        alert(`Уважаем${resp} ${firstName}, заявка создана`);
+        const ending = gender === 'female' ? 'ая' : 'ый';
+        alert(`Уважаем${ending} ${firstName}, заявка создана`);
     }
 }
-
-
-/*
-Я сделал два решения, потому что не знаю какое лучше,
-но мне все же первое больше нравится. И хотел бы узнать
-какая реализация, по вашему мнению, лучше.
-*/
-
-
-/*
-function validate(data) {
-    const { login, password, confirmPassword,
-        license, firstName, gender } = data;
-
-    if (!login || !password) {
-        alert('Укажите логин/пароль');
-    } else if (login ===  "beeline" || login ===  "beeinterns" || login ===  "bee") {
-        alert('Этот логин уже занят, выберите пожалуйста другой');
-    } else if (password.length < 6 ) {
-        alert('Пароль должен быть длинной не менее 6 символов');
-    } else if (password !== confirmPassword) {
-        alert('Пароли должны совпадать');
-    } else if (!firstName) {
-        alert('Укажите пожалуйста свое имя');
-    } else if (!gender) {
-        alert('Выберите пожалуйста пол');
-    } else if (!license) {
-        alert('Необходимо согласие');
-    } else if (gender == 'female') {
-        alert(`Уважаемая ${firstName}, заявка создана`);
-    } else if (gender == 'male') {
-        alert(`Уважаемый ${firstName}, заявка создана`);
-    }
-}*/
